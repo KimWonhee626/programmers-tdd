6a8aa9b09dd7ef754a530b4c9fbe6cc56fc80dfb
KimWonhee626 <kwh7739@gmail.com>
2023-03-10 16:07:00 +0900

    더하기, 빼기

--- src/main/java/org/example/polynomial/Polynomial.java
+++ src/main/java/org/example/polynomial/Polynomial.java
@@ -6,18 +6,26 @@ public class Polynomial {
     String input;
     Stack<Integer> intStack;
 
-    public Polynomial(String input){
+    public Polynomial(String input) {
         this.input = input;
         this.intStack = new Stack<Integer>();
 
     }
-    
-    public int calc(){
+
+    public static int calc(String exp) {
         int result = 0;
 
-        public
+        String[] bits = exp.split(" ");
 
+        int a = Integer.parseInt(bits[0]);
+        int b = Integer.parseInt(bits[2]);
+        String op = bits[1];
 
+        if (op.equals("+"))
+            result = a + b;
+        else if (op.equals("-"))
+            result = a - b;
         return result;
     }
+
 }
--- src/test/java/org/example/polynomial/PolynomialTests.java
+++ src/test/java/org/example/polynomial/PolynomialTests.java
@@ -1,22 +1,27 @@
 package org.example.polynomial;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PolynomialTests {
     @Test
-    public void getSplitInfo() {
-        assertEquals(6, new Polynomial("3 + 3").calc());
+    @DisplayName("3 + 3 = 6")
+    public void t1() {
+        assertThat(Polynomial.calc("3 + 3")).isEqualTo(6);
     }
 
     @Test
-    public void getSplitInfo_3() {
-        assertEquals(12, new Polynomial("3 * 3 + 3").calc());
+    @DisplayName("50 + 300 = 350")
+    public void t2() {
+        assertThat(Polynomial.calc("50 + 300")).isEqualTo(350);
     }
 
     @Test
-    public void getSplitInfo_4() {
-        assertEquals(18, new Polynomial("(3 + 3) * 3").calc());
+    @DisplayName("50 - 30 = 20")
+    public void t3() {
+        assertThat(Polynomial.calc("50 - 30")).isEqualTo(20);
     }
 }
