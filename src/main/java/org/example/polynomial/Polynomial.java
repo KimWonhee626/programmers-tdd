d3081fbf9b522b0850ea360e0d20841012ffb957
KimWonhee626 <kwh7739@gmail.com>
2023-03-14 16:41:46 +0900

    10 * 20 + 10 + 5 * 2 == 220

--- src/main/java/org/example/polynomial/Polynomial.java
+++ src/main/java/org/example/polynomial/Polynomial.java
@@ -1,6 +1,7 @@
 package org.example.polynomial;
 
-import java.util.Stack;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Polynomial {
     public Polynomial(String exp) {
@@ -8,31 +9,46 @@ public class Polynomial {
     }
 
     public static int calc(String exp) {
-        boolean needToMulti = exp.contains("*");
-        boolean needToPlus = exp.contains("+");
+
+        // 매개변수에 단일항이 들어오면 그대로 리턴
+        if (!(exp.contains(" "))) {
+            return Integer.parseInt(exp);
+        }
+
+        boolean needToMulti = exp.contains(" * ");
+        boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
+
         boolean needToCompound = needToMulti && needToPlus;
 
         if (needToCompound) {
+            exp = exp.replaceAll("- ", "+ -");
             String[] bits = exp.split(" \\+ ");
 
-            return Integer.parseInt(bits[0]) + calc(bits[1]);
-        }
-        else if (needToPlus) {
+            String newExp = Arrays.stream(bits)
+                    .mapToInt(Polynomial::calc)
+                    .mapToObj(e -> e + "") // String으로 변경
+                    .collect(Collectors.joining(" + "));
+
+            return calc(newExp);
+        } else if (needToPlus) {
             exp = exp.replaceAll("- ", "+ -");
 
             String[] bits = exp.split(" \\+ ");
             int sum = 0;
+
             for (int i = 0; i < bits.length; i++) {
                 sum += Integer.parseInt(bits[i]);
             }
+
             return sum;
-        }
-        else if (needToMulti) {
+        } else if (needToMulti) {
             String[] bits = exp.split(" \\* ");
             int sum = 1;
+
             for (int i = 0; i < bits.length; i++) {
                 sum *= Integer.parseInt(bits[i]);
             }
+
             return sum;
         }
 
--- src/test/java/org/example/polynomial/PolynomialTests.java
+++ src/test/java/org/example/polynomial/PolynomialTests.java
@@ -26,7 +26,7 @@ public class PolynomialTests {
     }
 
     @Test
-    @DisplayName("10 - 20 + 30 == 2 0")
+    @DisplayName("10 - 20 + 30 == 20")
     public void t4() {
         assertThat(Polynomial.calc("10 - 20 + 30")).isEqualTo(20);
     }
@@ -49,5 +49,34 @@ public class PolynomialTests {
         assertThat(Polynomial.calc("10 + 5 * 2")).isEqualTo(20);
     }
 
+    @Test
+    @DisplayName("20 + 10 + 5 * 2 == 40")
+    public void t8() {
+        assertThat(Polynomial.calc("20 + 10 + 5 * 2")).isEqualTo(40);
+    }
+
+    @Test
+    @DisplayName("10 * 20 - 20 * 30 == -400")
+    public void t9() {
+        assertThat(Polynomial.calc("10 * 20 - 20 * 30")).isEqualTo(-400);
+    }
+
+    @Test
+    @DisplayName("10 * 20 + 10 + 5 * 2 == 220")
+    public void t10() {
+        assertThat(Polynomial.calc("10 * 20 + 10 + 5 * 2")).isEqualTo(220);
+    }
+
+    @Test
+    @DisplayName("10 * -20 == -200")
+    public void t11() {
+        assertThat(Polynomial.calc("10 * -20")).isEqualTo(-200);
+    }
+
+    @Test
+    @DisplayName("(10 + 20) == 30")
+    public void t12() {
+        assertThat(Polynomial.calc("(10 + 20)")).isEqualTo(30);
+    }
 
 }
